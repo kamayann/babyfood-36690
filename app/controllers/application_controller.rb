e928b0a375a2c118ef373a20c6dc1812401c94a3
kamayann <2520.lfp@gmail.com>
2022-01-24 15:39:51 +0900

    子ども情報編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :relationship])
   end
-
-  def after_sign_in_path_for(resource)
-    new_baby_path
-  end
+  
 end
 
 
--- app/controllers/babies_controller.rb
+++ app/controllers/babies_controller.rb
@@ -1,6 +1,8 @@
 class BabiesController < ApplicationController
+  before_action :move_index, :only=>[:edit]
 
   def index
+    @babies = Baby.where(user_id: current_user.id)
   end
 
   def new
@@ -16,9 +18,30 @@ class BabiesController < ApplicationController
     end
   end
 
+  def edit
+    
+  end
+
+  def update
+    @baby = Baby.find(params[:id])
+    if @baby.update(baby_params)
+      redirect_to babies_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def baby_params
     params.require(:baby).permit(:nickname, :birth_day).merge(user_id: current_user.id)
   end
+
+  def move_index
+    @baby = Baby.find(params[:id])
+    unless current_user.id == @baby.user_id
+      redirect_to babies_path
+    end
+  end
+
 end
