d817b214e02923ea7f5c779c0a8e55a19a4fc533
kamayann <2520.lfp@gmail.com>
2022-01-25 21:44:45 +0900

    日本仕様に変更

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Babyfood36690
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     config.generators do |g|
       g.stylesheets false
