0b1af18a55eeb8193d4ecae3a80fa5778662273f
kamayann <2520.lfp@gmail.com>
2022-03-08 12:31:35 +0900

    FactoryBotのデータ

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    relationship_id       {2}
-    password              {'000000'}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 {Faker::Internet.free_email}
+    relationship_id       {Faker::Number.between(from: 2, to: 10)}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
   end
 end
