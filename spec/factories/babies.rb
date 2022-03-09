804671b034562cfa274c0eb28a2c4242408f0df4
kamayann <2520.lfp@gmail.com>
2022-03-09 09:18:34 +0900

    baby_userモデルの単体テスト

--- spec/factories/babies.rb
+++ spec/factories/babies.rb
@@ -2,5 +2,12 @@ FactoryBot.define do
   factory :baby do
     nickname       { Faker::Name.initials(number: 2) }
     birth_day      { Faker::Date.birthday}
+
+    trait :with_user do
+      after(:create) do |baby|
+        user = FactoryBot.create(:user)
+        create_list(:baby_user, 1, baby: baby, user: user)
+      end
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     nickname              { Faker::Name.initials(number: 2) }
     email                 {Faker::Internet.free_email}
     relationship_id       {Faker::Number.between(from: 2, to: 10)}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'a1' + Faker::Internet.password(min_length: 4)}
     password_confirmation {password}
   end
 end
