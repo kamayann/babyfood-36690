906522be28a94bf1fdf1a8a4d8e510277e72a406
kamayann <2520.lfp@gmail.com>
2022-03-25 22:25:24 +0900

    usersコントローラー単体テストの立ち上げ

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "UsersController", type: :request do
+
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  describe "GET #edit" do
+    
+  end
+end
