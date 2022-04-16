949a927cc817b42e0ce41a5b597c53e7715e1de5
kamayann <2520.lfp@gmail.com>
2022-04-16 21:31:52 +0900

    userコントローラーのテスト項目①

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,6 +7,13 @@ RSpec.describe "UsersController", type: :request do
   end
 
   describe "GET #edit" do
-    
+    it "editアクションにリクエストすると正常にレスポンスが返ってくる" do
+    end
+    it "editアクションにリクエストするとレスポンスに登録済みのユーザー名が存在する" do
+    end
+    it "editアクションにリクエストするとレスポンスに登録済みのニックネームが存在する" do
+    end
+    it "editアクションにリクエストするとレスポンスに登録済みのお子さんとのご関係が存在する" do
+    end
   end
 end
